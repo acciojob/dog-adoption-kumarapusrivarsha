e899ebf9daa75267919f8020b57b610fd3ce45a9
kumarapusrivarsha <134908628+kumarapusrivarsha@users.noreply.github.com>
2026-07-19 12:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+
+console.log("Dog Adoption Page Loaded Successfully");
+
+
+// Image click message
+
+const images = document.querySelectorAll("img");
+
+
+images.forEach(function(image){
+
+    image.addEventListener("click", function(){
+
+        console.log("Opening information about " + image.alt);
+
+    });
+
+});
